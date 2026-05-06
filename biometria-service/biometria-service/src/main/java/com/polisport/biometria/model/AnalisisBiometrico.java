9a4aa9aa346c6a2e05395d77627ea9e668f302a6
Felipe Barra <fbarra1994@gmail.com>
2026-05-06 18:36:20 -0400

    Microservicio biometria funcionando con jpa y mysql

--- biometria-service/biometria-service/src/main/java/com/polisport/biometria/model/AnalisisBiometrico.java
+++ biometria-service/biometria-service/src/main/java/com/polisport/biometria/model/AnalisisBiometrico.java
@@ -1,10 +1,7 @@
 package com.polisport.biometria.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "analisis_biometrico")
@@ -13,21 +10,18 @@ import lombok.Setter;
 @Getter
 @Setter
 public class AnalisisBiometrico {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long atletaId;
     private String fecha;
-    //composicion corporal
     private double peso;
     private double altura;
     private double imc;
     private double porcentajeGrasa;
     private double masaMuscular;
-
-    //indicador de rendimiento
     private double vo2Max;
     private int frecuenciaCardiacaReposo;
     private double indicadorRendimiento;
-
 }
