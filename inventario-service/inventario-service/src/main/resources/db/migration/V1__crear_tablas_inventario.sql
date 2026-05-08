057e7315b3b6965cd2ff1716ac4d2bfc0dc8da4b
Felipe Barra <fbarra1994@gmail.com>
2026-05-07 21:15:48 -0400

    codigo funcionando bases de datos creadas microservicios funcionando flyway sigue sin detectarse

--- inventario-service/inventario-service/src/main/resources/db/migration/V1__crear_tablas_inventario.sql
+++ inventario-service/inventario-service/src/main/resources/db/migration/V1__crear_tablas_inventario.sql
@@ -1,24 +1,8 @@
-CREATE TABLE IF NOT EXISTS instalacion (
-                                           id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                                           nombre VARCHAR(255),
-    tipo VARCHAR(255),
-    capacidad INT NOT NULL,
-    disponible BIT NOT NULL,
-    estado VARCHAR(255)
-    );
+CREATE TABLE instalacion (
+                             id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                             nombre VARCHAR(100) NOT NULL,
+                             ubicacion VARCHAR(100) NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS inventario (
-                                          id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                                          nombre VARCHAR(255),
-    cantidad INT NOT NULL,
-    estado VARCHAR(255),
-    instalacion_id BIGINT
-    );
-
-INSERT INTO instalacion (nombre, tipo, capacidad, disponible, estado)
-VALUES ('Cancha Principal', 'Cancha', 100, 1, 'Bueno'),
-       ('Gimnasio A', 'Gimnasio', 30, 1, 'Bueno');
-
-INSERT INTO inventario (nombre, cantidad, estado, instalacion_id)
-VALUES ('Balones de futbol', 20, 'Bueno', 1),
-       ('Conos de entrenamiento', 15, 'Bueno', 2);
+INSERT INTO instalacion (nombre, ubicacion) VALUES ('Gimnasio Techado', 'Sector A');
+INSERT INTO instalacion (nombre, ubicacion) VALUES ('Cancha de Fútbol 1', 'Sector B');
