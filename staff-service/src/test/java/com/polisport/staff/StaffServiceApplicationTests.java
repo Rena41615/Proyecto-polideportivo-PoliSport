739c84b94438db0127d7c46833ba6ccb1880f903
jiuoqwvek <noemiaolanicaiguan@gmail.com>
2026-05-11 00:44:15 -0400

    feat: implementar staff-service y migracion flyway

---
+++ staff-service/src/test/java/com/polisport/staff/StaffServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.polisport.staff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class StaffServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
